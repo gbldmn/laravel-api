f747af19d90fbbccdf5672827a87abecb356155a
glbdmn <gabrieledamiani@hotmail.it>
2023-06-28 17:51:09 +0200

    fine esercizio

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,15 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
 
+
+// chiamata api per vue
+
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        // $projects = Project::all();
+
+        $projects = Project::with( 'type', 'technologies' )->get();
 
         return response()->json([
-            'succes' => 'true',
-            'projects' => '$projects'
+            'succes' => true,
+            'projects' => $projects
         ]);
     }
 }
